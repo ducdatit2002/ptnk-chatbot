44d513f2837801a1ea3d02bbdcfa1fd45e8c6dcf
ducdatit2002 <ducdatit2002@gmail.com>
2026-04-18 16:47:02 +0700

    Update

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Chatbot Trường Phổ thông Năng khiếu - ĐHQG-HCM",
-  description: "Chatbot hỏi đáp thân thiện cho học sinh lớp 9 và phụ huynh về Trường Phổ thông Năng khiếu - ĐHQG-HCM.",
+  description: "Chatbot Trường Phổ thông Năng khiếu - ĐHQG-HCM.",
   icons: {
     icon: "/logo-ptnk.png",
     apple: "/logo-ptnk.png",
